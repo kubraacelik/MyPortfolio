58eacfd336e4ded00870034f94562b39fa6cdeeb
kubraacelik <kkbra.celik92@gmail.com>
2024-03-21 23:09:22 +0300

    added FaTimes and FaBar

---
+++ myportfolio/src/components/Navbar.js
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  const [color, setColor] = useState(false);
+
+  // scroll'da 100'den fazla aşağı inersek navbar'ın bg'si siyah olur
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+
+  // scroll olayı her tetiklendiğinde changeColor fonksiyonunun çağrılmasını sağlar.
+  window.addEventListener("scroll", changeColor);
+
+  return (
+    <div className={color ? "header header-bg" : "header"}>
+      <Link to="/">
+        <h1>PORTFOLYO</h1>
+      </Link>
+
+      {/* menüye tıklarsan aktif olup li'ler ekrana gelir */}
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
