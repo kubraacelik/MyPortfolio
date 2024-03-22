611914aaf5745a494c6cfee0de5bfeac788315f9
kubraacelik <kkbra.celik92@gmail.com>
2024-03-22 17:54:26 +0300

    added img and title for Project

---
+++ myportfolio/src/components/HeroImg2.js
@@ -0,0 +1,16 @@
+import './HeroImg2.css'
+
+import React from 'react'
+
+const HeroImg2 = () => {
+  return (
+    <div className='hero-img'>
+        <div className="heading">
+            <h1>PROJECTS</h1>
+            <p>Some of My Recent Works</p>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg2
