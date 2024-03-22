2d0f29864b6049982a1df8b8817d5aa42e024d3a
kubraacelik <kkbra.celik92@gmail.com>
2024-03-22 17:54:31 +0300

    added img and title for About

---
+++ myportfolio/src/components/HeroImg3.js
@@ -0,0 +1,18 @@
+import './HeroImg3.css'
+
+import React from 'react'
+
+const HeroImg3 = () => {
+  return (
+    <div className='img'>
+    <div className="head">
+        <h1>ABOUT</h1>
+        <p>I am a senior undergraduate student of computer engineering.</p>
+        <p>I am interested in frontend.</p>
+        <p>Skills : HTML, CSS, Javascript and React JS</p>
+    </div>
+</div>
+  )
+}
+
+export default HeroImg3
